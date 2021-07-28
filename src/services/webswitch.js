488945a31a25abd6f8c5434aff083249589c8c69
tyson <tyson7757@comcast.net>
2021-07-28 00:33:52 -0500

    fixes

--- src/services/webswitch.js
+++ src/services/webswitch.js
@@ -1,8 +1,89 @@
-export default async (event, observer) => {
+// export default async (event, observer) => {
+//   try {
+//     const publishEvent = (await import("microservices/webswitch")).publishEvent;
+//     publishEvent(event, observer);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+/**
+ * WEBSWITCH (c)
+ * websocket clients connect to a common server,
+ * which broadcasts any messages it receives.
+ */
+"use strict";
+
+import WebSocket from "ws";
+import dns from "dns/promises";
+
+const FQDN = process.env.WEBSWITCH_HOST || "webswitch.aegis.dev";
+const PORT = 8062;
+const PATH = "/webswitch/broadcast";
+
+async function lookup(hostname) {
   try {
-    const publishEvent = (await import("microservices/webswitch")).publishEvent;
-    publishEvent(event, observer);
+    const result = await dns.lookup(hostname);
+    console.debug("server address", result, result.address);
+    return result.address;
   } catch (error) {
-    console.error(error);
+    console.warn("dns lookup", error);
   }
-};
+  return "localhost";
+}
+
+async function getHostName() {
+  const hostname = await lookup(FQDN);
+  return hostname ? hostname : "localhost";
+}
+
+/**@type import("ws/lib/websocket") */
+let ws;
+let hostname;
+
+export default async function publishEvent(event, observer) {
+  if (!event) return;
+
+  if (!hostname) hostname = await getHostName();
+  const serializedEvent = JSON.stringify(event);
+
+  function webswitch() {
+    console.debug("webswitch sending", event);
+
+    if (!ws) {
+      ws = new WebSocket(`ws://${hostname}:${PORT}${PATH}`);
+
+      ws.on("message", function (message) {
+        const event = JSON.parse(message);
+        if (event.eventName) {
+          observer.notify(event.eventName, event);
+        }
+      });
+
+      ws.on("open", function () {
+        ws.send(JSON.stringify("webswitch"));
+      });
+
+      ws.on("error", function (error) {
+        console.error("webswitchClient.on(error)", error);
+      });
+      return;
+    }
+
+    function send() {
+      if (ws.readyState) {
+        ws.send(serializedEvent);
+        return;
+      }
+      setTimeout(() => send(), 1000);
+    }
+
+    send();
+  }
+
+  try {
+    webswitch();
+  } catch (e) {
+    console.warn(publishEvent.name, e.message);
+  }
+}
