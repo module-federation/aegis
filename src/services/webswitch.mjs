229b8418ba40984ec2cdd29da2c2114e8fd0a09a
tyson <tyson7757@comcast.net>
2021-07-28 10:56:55 -0500

    webswitch

--- src/services/webswitch.mjs
+++ src/services/webswitch.mjs
@@ -1,33 +1,16 @@
+// export default async (event, observer) => {
+//   try {
+//     const publishEvent = (await import("microservices/webswitch")).publishEvent;
+//     publishEvent(event, observer);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
 /**
  * WEBSWITCH (c)
  * websocket clients connect to a common server,
  * which broadcasts any messages it receives.
- * 
- * 
-  // Server queries client to get its configuration so that it only sends 
-  // requests the client can fulfill vs blindly broadcasting.
-  //
-  // Also, server deduplicates responses by only sending to one of multiiple clients 
-  // possessing the same resources. 
-  // 
-  // Because WebSwitch understands the application architecture, there's no need for it to inspect
-  // the contents of incoming and outgoing messages. The routes are set up ahead of time based on 
-  // the client config.
-  //
-  // Server also performs load balancing, based on resource avalability, distribution 
-  // and performance. Poorly performing clients/subnets are disused.
-  // 
-  // Server analytics give recommendations about physical deployment options
-  // and application configuration changes to optmize performance.
-  // 
-  //
-  // Server automation acts on these recommendations, allowing the app to be dynmically redeployed
-  // to another network or datacenter or scaled out or reconfigured, e.g. so chatty or busy commponents 
-  // creating hot spots on the network are redeployed to run together in a single process, where they can 
-  // communicate directly via memory instead, or workload is dynamically redistributed across additional
-  // nodes, including moving any data that's needed and streaming component code to instancess where it 
-  // isn't deployed.
-  //
  */
 "use strict";
 
@@ -38,7 +21,7 @@ const FQDN = process.env.WEBSWITCH_HOST || "webswitch.aegis.dev";
 const PORT = 8062;
 const PATH = "/webswitch/broadcast";
 
-async function lookup(hostname) {
+async function getHostAddress(hostname) {
   try {
     const result = await dns.lookup(hostname);
     console.debug("server address", result, result.address);
@@ -46,33 +29,29 @@ async function lookup(hostname) {
   } catch (error) {
     console.warn("dns lookup", error);
   }
-  return null;
-}
-
-async function getHostName() {
-  const hostname = await lookup(FQDN);
-  return hostname ? hostname : "localhost";
+  return "localhost";
 }
 
 /**@type import("ws/lib/websocket") */
 let ws;
+let hostAddress;
 
-export async function publishEvent(event) {
+export default async function publishEvent(event, observer) {
   if (!event) return;
 
-  const hostname = await getHostName();
-  const serializedEvent = JSON.stringify(event);
+  if (!hostAddress) hostAddress = await getHostAddress(FQDN);
 
   function webswitch() {
-    console.debug("webswitch sending", event);
-
     if (!ws) {
-      ws = new WebSocket(`ws://${hostname}:${PORT}${PATH}`);
+      ws = new WebSocket(`ws://${hostAddress}:${PORT}${PATH}`);
 
-      ws.on("message", function (message) {
-        console.debug(message);
-        const event = JSON.parse(message);
-        // console.debug(event);
+      ws.on("message", async function (message) {
+        const eventData = JSON.parse(message);
+        if (eventData.eventName && observer) {
+          await observer.notify(eventData.eventName, eventData);
+          return;
+        }
+        console.warn("eventName or observer missing", message);
       });
 
       ws.on("open", function () {
@@ -86,6 +65,7 @@ export async function publishEvent(event) {
     }
 
     function send() {
+      const serializedEvent = JSON.stringify(event);
       if (ws.readyState) {
         ws.send(serializedEvent);
         return;
@@ -102,5 +82,3 @@ export async function publishEvent(event) {
     console.warn(publishEvent.name, e.message);
   }
 }
-
-publishEvent("webswitch");
