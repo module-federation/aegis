03737e18295392094a5a0ce7e7bcb2f0605c5e26
tyson <tyson7757@comcast.net>
2021-08-05 07:08:35 -0500

    was

---
+++ src/wasm/domain/compensate.ts
@@ -0,0 +1,53 @@
+import domainEvents from "./domain-events";
+
+async function reportStatus(status, eventFn, model) {
+  const result = { compensateResult: status };
+  await model.emit(eventFn(model), result);
+  await model.update(result);
+}
+
+/**
+ * Steps through the sequence of port calls
+ * in LIFO order executing their undo functions.
+ * @param {import('.').Model} model
+ * @returns {function():Promise<void>}
+ */
+export default async function compensate(model) {
+  try {
+    const portFlow = model.getPortFlow();
+    const ports = model.getPorts();
+
+    await model.emit(domainEvents.undoStarted(model), "undo starting");
+
+    const undoModel = await Promise.resolve(
+      portFlow.reduceRight(async function (model, port, index, arr) {
+        if (ports[port].undo) {
+          console.info("calling undo on port: ", port);
+
+          try {
+            return model.then(async function (model) {
+              await ports[port].undo(model);
+
+              return model.update({
+                [model.getKey("portFlow")]: arr.splice(0, index),
+              });
+            });
+          } catch (error) {
+            console.error(compensate.name, error.message);
+          }
+        }
+        return model;
+      }, model.update({ compensate: true }))
+    );
+
+    if (undoModel.getPortFlow().length > 0) {
+      await reportStatus("INCOMPLETE", domainEvents.undoFailed, undoModel);
+      return;
+    }
+
+    await reportStatus("COMPLETE", domainEvents.undoWorked, undoModel);
+  } catch (error) {
+    await model.emit(domainEvents.undoFailed(model), error.message);
+    console.error(compensate.name, error.message);
+  }
+}
