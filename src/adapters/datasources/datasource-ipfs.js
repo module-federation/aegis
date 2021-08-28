f5de4c3c6bac2cb8abf4d191737b0ba6e083be90
trmidboe <trmibdoe@gmail.com>
2021-08-28 10:08:14 -0500

    version

--- src/adapters/datasources/datasource-ipfs.js
+++ src/adapters/datasources/datasource-ipfs.js
@@ -1,8 +1,7 @@
-import ipfs from "ipfs-core/src/components";
 import fs from "fs";
 import { DataSourceFile } from ".";
-const lockfile = require('proper-lockfile');
-
+import IPFS from "ipfs-core";
+const ipfs = IPFS.create();
 
 /**
  * Storage on the distributed web {@link https://ipfs.io}
@@ -11,8 +10,6 @@ export class DataSourceIpfs extends DataSourceFile {
   constructor(dataSource, factory, name) {
     super(dataSource, factory, name);
     const self = this;
-    lockfile.unlock('/Users/tyson/.jsipfs/repo.lock');
-    ipfs.create().then(node => self.node = node);
   }
 
   async save(id, data) {
@@ -35,33 +32,25 @@ export class DataSourceIpfs extends DataSourceFile {
   }
 
   async readFile(hydrate) {
-    let data;
     try {
-      const stream = this.node.cat(this.cid);
+      let data;
+      const stream = ipfs.cat(this.cid);
       for await (const chunk of stream) {
         // chunks of data are returned as a Buffer, convert it back to a string
         data += chunk.toString();
       }
       return hydrate(new Map(JSON.parse(data), this.revive));
     } catch (e) {
-      lockfile.unlock('/Users/tyson/.jsipfs/repo.lock');
+      console.error(e)
     }
   }
 
   async writeFile() {
     // add your data to to IPFS - this can be a string, a Buffer,
     // a stream of Buffers, etc
-    const results = this.node.add(JSON.stringify([...this.dataSource]));
-    // we loop over the results because 'add' supports multiple
-    // additions, but we only added one entry here so we only see
-    // one log line in the output
-    for await (const { cid } of results) {
-      // CID (Content IDentifier) uniquely addresses the data
-      // and can be used to get it again.
-      console.log(cid.toString());
-    }
-    this.cid = cid;
-    //save the CID value
-    fs.writeFileSync(this.name, cid.toString());
+    const { cid } = ipfs.add(JSON.stringify([...this.dataSource]))
+
+    fs.writeFileSync(this.file, cid.toString())
+    this.cid = cid
   }
 }
--- src/domain/domain-events.js
+++ src/domain/domain-events.js
@@ -5,18 +5,19 @@ const domainEvents = {
   externalCacheRequest: modelName => `externalCacheRequest_${modelName}`,
   internalCacheResponse: modelName => `internalCacheResponse_${modelName}`,
   externalCacheResponse: modelName => `externalCacheResponse_${modelName}`,
-  cacheCreationEvent: modelName => `cacheCreationEvent_${modelName}`,
+  cacheCreateEvent: modelName => `cacheCreateEvent_${modelName}`,
+  cacheUpdateEvent: modelName => `cacheUpdateEvent_${modelName}`,
+  unauthorizedCommand: model => `unauthorizedCommand_${model.getName()}`,
+  undoStarted: model => `undoStart_${model.getName()}`,
+  undoFailed: model => `undoFailed_${model.getName()}`,
+  undoWorked: model => `undoWorked_${model.getName()}`,
+  addModel: modelName => `addModel_${modelName}`,
+  editModel: modelName => `editModel_${modelName}`,
   portTimeout: (model, port) => `portTimeout_${port}_${model.getName()}`,
   portRetryFailed: (model, port) =>
     `portRetryFailed_${port}_${model.getName()}`,
   portRetryWorked: (model, port) =>
     `portRetryWorked_${port}_${model.getName()}`,
-  undoStarted: model => `undoStart_${model.getName()}`,
-  undoFailed: model => `undoFailed_${model.getName()}`,
-  undoWorked: model => `undoWorked_${model.getName()}`,
-  unauthorizedCommand: model => `unauthorizedCommand_${model.getName()}`,
-  addModel: modelName => `addModel_${modelName}`,
-  editModel: modelName => `editModel_${modelName}`,
 };
 
 export default domainEvents;
