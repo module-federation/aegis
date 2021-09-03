15537d83b6465ebef9150a9fccdd086756effa78
trmidboe <trmibdoe@gmail.com>
2021-09-03 06:33:41 -0500

    fix validate wasm

--- src/adapters/webassembly/wasm-import.js
+++ src/adapters/webassembly/wasm-import.js
@@ -134,7 +134,7 @@ export async function importWebAssembly (remoteEntry, type = 'model') {
       return wasm.then(inst =>
         fn(...ptrArray.map(p => inst.exports.__getString(p)))
       )
-    } else if (wasm.exports) {
+    } else if (wasm.exports?.__getString) {
       return fn(...ptr.map(p => wasm.exports.__getString(p)))
     } else {
       console.log('no ref to module')
