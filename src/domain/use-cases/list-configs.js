de9fa2c04a06e502217acc7a43b9d1350801f870
tysonrm <tyson.midboe@gmail.com>
2022-04-22 12:55:44 -0500

    observability

--- src/domain/use-cases/list-configs.js
+++ src/domain/use-cases/list-configs.js
@@ -24,13 +24,14 @@ export default function listConfigsFactory ({
       data: () =>
         modelName && isMainThread
           ? threadpools.getThreadPool(modelName).run(listConfigs.name, query)
-          : DataSourceFactory.listDataSources().map(k => ({
-              dsname: k,                                                                                                                                                        
-              records: DataSourceFactory.getDataSource(k).totalRecords()
+          : modelName
+          ? dsFact.getDataSource(modelName).listSync()
+          : dsFact.listDataSources().map(k => ({
+              dsname: k,
+              records: dsFact.getDataSource(k).totalRecords()
             })),
-            
       events: () =>
-        modelName && isMainThread                    
+        modelName && isMainThread
           ? threadpools.getThreadPool(modelName).run(listConfigs.name, query)
           : [...broker.getEvents()].map(([k, v]) => ({
               eventName: k,
