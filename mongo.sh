cd40fd395d35c1741e5fa7a8fdfd443cb39d4c1d
tyson <tyson.midboe@gmail.com>
2022-05-17 06:34:05 +0000

    reset

--- mongo.sh
+++ mongo.sh
@@ -1,13 +1,9 @@
 mkdir -p /tmp/mongodb && \
     cd /tmp/mongodb && \
-    wget -qOmongodb.tgz https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-5.0.2.tgz && \
-    tar xf mongodb.tgz && \
-    cd mongodb-* && \
     sudo cp bin/* /usr/local/bin/ && \
-    cd /workspace
     rm -rf /tmp/mongodb && \
-    sudo mkdir -p /data/db && \
-    sudo chown gitpod:gitpod -R /data/db
-    mongod
+    sudo mkdir -p /workspace/db && \
+    sudo chown gitpod:gitpod -R /workspace/db
+    mongod --dbpath /workspace/db
 
 
---
+++ start.sh
@@ -0,0 +1,3 @@
+# create symlink between aegis-host and aegis vs using npm  
+
+yarn link && cd ../aegis-host && yarn link @module-federation/aegis && yarn start
