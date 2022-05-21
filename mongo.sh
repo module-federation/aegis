6a1df807d68a586f92fa7f11100d4b0ca4bb3b10
tyson <tyson.midboe@gmail.com>
2022-05-21 11:49:38 +0000

    fix mongo script

--- mongo.sh
+++ mongo.sh
@@ -1,9 +1,8 @@
 mkdir -p /tmp/mongodb && \
     cd /tmp/mongodb && \
+    wget -qOmongodb.tgz https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-5.0.2.tgz && \
     sudo cp bin/* /usr/local/bin/ && \
-    rm -rf /tmp/mongodb && \
+    cd /workspace && rm -rf /tmp/mongodb && \    
     sudo mkdir -p /workspace/db && \
-    sudo chown gitpod:gitpod -R /workspace/db
+    sudo chown gitpod:gitpod -R /workspace/db && \
     mongod --dbpath /workspace/db
-
-
