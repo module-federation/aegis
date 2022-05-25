e1ae51e74b55cc81733618ebe91b6a857dab045f
tyson <tyson.midboe@gmail.com>
2022-05-25 23:04:20 +0000

    testing

--- mongo.sh
+++ mongo.sh
@@ -1,6 +1,9 @@
 mkdir -p /tmp/mongodb && \
     cd /tmp/mongodb && \
     wget -qOmongodb.tgz https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-5.0.2.tgz && \
+    tar xf mongodb.tgz
+    pwd && ls && \
+    cd mongodb-* && \
     sudo cp bin/* /usr/local/bin/ && \
     cd /workspace && rm -rf /tmp/mongodb && \    
     sudo mkdir -p /workspace/db && \
