f3d9742dacee9baf9b192569854979701f8b316e
tysonrm <trmidboe@gmail.com>
2022-07-27 19:06:59 -0500

    Changes to be committed: 	modified:   src/domain/use-cases/invoke-port.js

--- src/domain/use-cases/invoke-port.js
+++ src/domain/use-cases/invoke-port.js
@@ -1,8 +1,5 @@
 'use strict'
 
-import executeCommand from './execute-command'
-import async from '../util/async-error'
-import domainEvents from '../domain-events'
 import { isMainThread } from 'worker_threads'
 import AppError from '../util/app-error'
 
@@ -20,32 +17,30 @@ import AppError from '../util/app-error'
  * @param {ModelParam} param0
  * @returns {function():Promise<import("../domain/model").Model>}
  */
-export default function makeInvokePort ({
-  modelName,
-  models,
-  repository,
-  threadpool,
-  broker,
-  handlers = []
-} = {}) {
+export default function makeInvokePort ({ repository, threadpool } = {}) {
   /**
    *
-   * @param {{id:string,changes:object,command:string}} input
+   * @param {{id:string,model:import('..').Model,args:string[],port:string}} input
    * @returns
    */
   async function invokePort (input) {
     if (isMainThread) {
-      const model = await repository.find(input.id)
+      const { id, args, port } = input
+      const model = await repository.find(id)
       if (!model) {
         throw new Error('no such id')
       }
-      const updated = await threadpool.run(invokePort.name, input)
+      const updated = await threadpool.run(invokePort.name, {
+        model,
+        port,
+        args
+      })
       if (updated.hasError) throw new Error(updated.message)
       return updated
     } else {
       try {
         const { model, port, args } = input
-        return await model[port](args)
+        return await model[port](...args)
       } catch (e) {
         console.error(invokePort.name, e)
         return AppError(e)
