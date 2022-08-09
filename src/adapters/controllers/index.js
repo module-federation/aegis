64bdf1fd432e996662adac381fc2dc9bbd39e2ec
tysonrm <trmidboe@gmail.com>
2022-08-09 18:19:42 -0500

    Changes to be committed: 	modified:   src/adapters/controllers/index.js 	new file:   src/adapters/controllers/post-invoke-port.js

--- src/adapters/controllers/index.js
+++ src/adapters/controllers/index.js
@@ -11,7 +11,8 @@ const {
   listModels,
   removeModels,
   hotReload,
-  registerEvents
+  registerEvents,
+  invokePorts
 } = UseCases
 
 import postModelFactory from './post-model'
@@ -20,6 +21,7 @@ import getModelsFactory from './get-models'
 import getModelByIdFactory from './get-model-by-id'
 import deleteModelFactory from './delete-model'
 import getConfigFactory from './get-config'
+import postInvokePortFactory from './post-invoke-port'
 import makeLiveUpdate from './live-update'
 
 function make (useCases, controllerFactory) {
@@ -34,6 +36,7 @@ export const patchModels = () => make(editModels, patchModelFactory)
 export const getModels = () => make(listModels, getModelsFactory)
 export const getModelsById = () => make(findModels, getModelByIdFactory)
 export const deleteModels = () => make(removeModels, deleteModelFactory)
+export const postInvokePorts = () => make(invokePorts, postInvokePortFactory)
 export const liveUpdate = () => make(hotReload, makeLiveUpdate)
 export const getConfig = () => getConfigFactory(listConfigs())
 export const getRoutes = () => getUserRoutes()
