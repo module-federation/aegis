module-federation/aegis

commit cec144d60204ce3b6e628a46d9fc3816f90d4a8b

pass context into invoke-port

src/domain/use-cases/index.js
@@ -22,7 +22,7 @@ import makeServiceMesh from "./create-service-mesh.js"
 import { PortEventRouter } from "../event-router"
 import { isMainThread } from "worker_threads"
 import { hostConfig } from "../../config"
-import { requestContext } from "../util/async-context"
+import * as context from "../util/async-context"
 
 export const serviceMeshPlugin =
   hostConfig.services.activeServiceMesh ||
@@ -115,12 +115,13 @@ function buildOptions (spec, options) {
     modelName: spec.modelName,
     models: ModelFactory,
     broker: EventBrokerFactory.getInstance(),
-    handlers: spec.eventHandlers
+    handlers: spec.eventHandlers,
+    context,
   }
 
   if (isMainThread) {
     const ds = getDataSource(spec.modelName, options)
-    return {
+    return { 
       ...invariant,
       // main thread does not write to persistent store
       repository: ds,

src/domain/use-cases/invoke-port.js
@@ -23,6 +23,7 @@ export default function makeInvokePort ({
   threadpool,
   modelName,
   models,
+  context,
   authorize = async x => await x()
 } = {}) {
   async function findModelService (id = null) {
@@ -46,10 +47,14 @@ export default function makeInvokePort ({
         } 
 
         if(!port) {
-          const path = input.context.get('path');
-          const portTupleWithCustomPath = Object.entries(object1).filter((port) => port[1].path === path);
+          const specPorts = service.getPorts();
+          const path = context['requestContext'].getStore().get('path');
+          const portTupleWithCustomPath = Object.entries(specPorts).filter((port) => port[1].path === path);
           if(!portTupleWithCustomPath) {
             throw new Error('no port specified');
+          }
+          if(!service[portTupleWithCustomPath[0]]) {
+            throw new Error('no port found');
           }
           return await service[portTupleWithCustomPath[0]](input.jobData);
         }

commit 005fcbc7dfba00c7443c94d4152461918a222c0d

fix request context is undefined bug introduced in cec144d60204ce3b6e628a46d9fc3816f90d4a8b

src/domain/use-cases/index.js
@@ -129,7 +129,7 @@ function buildOptions (spec, options) {
       threadpool: getThreadPool(spec, ds),
       // if caller provides id, use it as key for idempotency
       async idempotent () {
-        return ds.find(requestContext.getStore().get('id'))
+        return ds.find(context.requestContext.getStore().get('id'))
       }
     }
   } else {