5819d3d223f84f8be3a818134cc0fe7277bcb791
Kyle <kyle.c.r.fahey@gmail.com>
2022-11-25 21:55:21 +0000

    fix duplicate calls

--- src/adapters/datasources/datasource-mongodb.js
+++ src/adapters/datasources/datasource-mongodb.js
@@ -60,6 +60,7 @@ export class DataSourceMongoDb extends DataSource {
       if(!this.connOpts.indexesHaveBeenRun && this.connOpts.indexes) {
         console.info(`running indexes for datasource ${this.name} with index values`, this.connOpts.indexes)
         await this.#createIndexes(client)
+        this.connOpts.indexesHaveBeenRun = true;
       }
 
       return client
