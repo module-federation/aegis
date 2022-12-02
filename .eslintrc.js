f8ddba0f3e3f4a560a6a54295149ce3a1c97eedb
Kyle <kyle.c.r.fahey@gmail.com>
2022-12-02 10:44:33 -0500

    add eslint recommended + prettier recommended to aegis core  (#11)
    * add eslint to aegis-core
    
    * add eslint recommended
    
    * update eslint env
    
    * update eslint parser options to allow modules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,18 @@
 module.exports = {
   env: {
     browser: true,
+    es6: true,
+    node: true,
+    commonjs: true,
     es2020: true,
   },
-  extends: ["standard", "plugin:prettier/recommended"],
   parserOptions: {
     ecmaVersion: 11,
-    sourceType: "module",
+    sourceType: 'module',
+    ecmaFeatures: {
+      modules: true,
+    },
   },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   rules: {},
 };
