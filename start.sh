484a92019ee3dd2ead952c84e978650a5f60d948
tyson <tyson.midboe@gmail.com>
2023-01-04 16:18:55 +0000

    fix wasm build

--- start.sh
+++ start.sh
@@ -1,8 +1,12 @@
 cd ../aegis-app
 nohup node repo.js &
 nohup node repo.js 8001 cache &
-cp ../aegis/wasm/build/release.wasm ../aegis-app/dist/main.wasm
-cd ../aegis
+cd ../aegis/wasm
+yarn build
+cd ../aegis/wasm
+yarn build
+yarn deploy
+cd ..
 yarn link
 cd ../aegis-host
 yarn link @module-federation/aegis
